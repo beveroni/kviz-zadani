290506e80f40d47791dd94964bb14a4846c79f4e
Veronika Bendova <svechovaver@gmail.com>
2023-07-28 18:25:01 +0200

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('funguju!');
+const questionElm = document.querySelector('.question');
+const question =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+
+questionElm.textContent = question;
+
+const userAnswer = prompt(question);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = userAnswer;
+
+const answerElm = document.querySelector('.answer');
+
+if (userAnswer.toLowerCase() === 'Bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
